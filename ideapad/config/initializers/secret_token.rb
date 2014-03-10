1bc4c3e753ee672c0cf3d6a615a0a0dd4acac2bf
zs <baloghzsof@gmail.com>
2014-03-10 15:18:29 +0100

    Generated new rails project.

---
+++ ideapad/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideapad::Application.config.secret_token = 'dd7c08856796bf960df4d31f259427e8c3001e2007e17dafbf9b9ebed2cd5429dc09164a4d3e33ae689b6fd135ec2abc62b09942a4b95fb3c6c7b8bdd62e9c8b'

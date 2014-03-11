7e9ae98d17018c6113fcfae9ed6b7b82fbdbd202
zs <baloghzsof@gmail.com>
2014-03-11 07:09:52 +0100

    Save user when creating an idea.

--- ideapad/app/controllers/ideas_controller.rb
+++ ideapad/app/controllers/ideas_controller.rb
@@ -44,6 +44,8 @@ class IdeasController < ApplicationController
   def create
     @idea = Idea.new(params[:idea])
 
+    @idea.user = current_user
+
     respond_to do |format|
       if @idea.save
         format.html { redirect_to @idea, notice: 'Idea was successfully created.' }

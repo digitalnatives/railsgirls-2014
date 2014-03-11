3010d8d3f10e825c30e099bde610be8eb948f131
zs <baloghzsof@gmail.com>
2014-03-11 09:04:11 +0100

    Implemented creator_unicorn_img idea helper.

--- ideapad/app/helpers/ideas_helper.rb
+++ ideapad/app/helpers/ideas_helper.rb
@@ -1,2 +1,10 @@
 module IdeasHelper
+
+  def creator_unicorn_img(idea, size=64)
+    if idea.user.present?
+      image_tag "http://unicornify.appspot.com/avatar/#{idea.user.email_md5}?s=#{size}",
+                alt: idea.user.email, title: idea.user.email
+    end
+  end
+
 end

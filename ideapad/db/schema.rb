af140225cddb0d641a71fb1542f35ec5ccead81e
zs <baloghzsof@gmail.com>
2014-03-11 07:07:25 +0100

    Add user_id column to ideas.ű

--- ideapad/db/schema.rb
+++ ideapad/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140310153638) do
+ActiveRecord::Schema.define(:version => 20140311060629) do
 
   create_table "ideas", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
